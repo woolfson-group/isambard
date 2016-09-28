8724c0f1bedc2c026cec24df24ac83cb104f1edc
Gail Bartlett <chgjb@bristol.ac.uk>
2016-09-28 16:19:54 +0100

    Added find_pi_pi_interactions test cases

--- unit_tests/test_interactions.py
+++ unit_tests/test_interactions.py
@@ -82,5 +82,19 @@ class CationPiInteractionFinderTestCase(unittest.TestCase):
         test_pdb = isambard.ampal.convert_pdb_to_ampal(test_path)
         self.assertEqual(len(isambard.ampal.interactions.find_cation_pi_interactions(test_pdb)),1)
 
+class PiPiInteractionFinderTestCase(unittest.TestCase):
+    """Test PiPi interaction finding mechanism"""
+
+    def test_3qy1(self):
+        test_path=os.path.join(os.path.dirname(isambard.__file__), 'unit_tests','testing_files','3qy1.pdb')
+        test_pdb = isambard.ampal.convert_pdb_to_ampal(test_path)
+        face,edge = isambard.ampal.interactions.find_pi_pi_interactions(test_pdb)
+        self.assertEqual(len(edge),2)
+    def test_2ebo(self):
+        test_path=os.path.join(os.path.dirname(isambard.__file__), 'unit_tests','testing_files','2ebo_1.mmol')
+        test_pdb = isambard.ampal.convert_pdb_to_ampal(test_path)
+        face,edge = isambard.ampal.interactions.find_pi_pi_interactions(test_pdb)
+        self.assertEqual(len(edge),1)
+        
 if __name__ == '__main__':
     unittest.main()
