d6dacb2dcd68a2165143ad0d1dbd242bf2744d70
Jack Heal <jack.heal@bristol.ac.uk>
2016-11-15 15:53:03 +0000

    Added wza cif file as testing file, and added test for knobs_into_holes that uses this test file

--- unit_tests/test_knobs_into_holes.py
+++ unit_tests/test_knobs_into_holes.py
@@ -9,6 +9,8 @@ class KnobGroupTestCase(unittest.TestCase):
     def setUp(self):
         test_file = os.path.join('unit_tests', 'testing_files', '2ebo_1.mmol')
         self.test_assembly = isambard.ampal.convert_pdb_to_ampal(test_file)
+        test_file = os.path.join('unit_tests', 'testing_files', '2j58_1.cif')
+        self.test_assembly_2 = isambard.add_ons.convert_cif_to_ampal(test_file)
 
     def test_number_of_kihs(self):
         """ Test there are 38 kihs found at 7.0 cutoff for 2ebo. """
@@ -19,3 +21,9 @@ class KnobGroupTestCase(unittest.TestCase):
         for i in range(len(a.helices)):
             kihs_locations += str(len([x for x in kg if x.knob_helix.number == i]))
         self.assertTrue(int(kihs_locations) == 914804714)
+
+    def test_kihs_at_large_cutoffs(self):
+        for scut in [7.0, 7.5, 8.0, 8.5, 9.0, 9.5, 10.0]:
+            a = self.test_assembly
+            kg = isambard.add_ons.knobs_into_holes.KnobGroup.from_helices(a, cutoff=scut)
+            self.assertEqual(len(kg), len(set([str(x) for x in kg])))
