fe21964550c6469f520c5bf4ee2c5694e08c9a9a
Chris Wells Wood <cwwoodesq@gmail.com>
2016-11-24 15:46:48 +0000

    Tweaked 4HB class.

--- isambard_dev/ampal/specifications/assembly_specs/membrane_4hb.py
+++ isambard_dev/ampal/specifications/assembly_specs/membrane_4hb.py
@@ -50,9 +50,10 @@ def find_tilt_to_mem_angle(helix, membrane_hp_thickness):
 class Membrane4HelixBundle(Assembly):
     default_sh_rot = (0, 90, 180, 270)
 
-    def __init__(self, aas=(20, 20, 20, 20), aps=(False, False, False, False), radii=(5, 5, 5, 5),
-                 ah_rotations=(0, 0, 0, 0), sh_rotations=(0, 0, 0, 0), lmp_rotations=(0, 0, 0, 0),
-                 zshifts=(0, 0, 0, 0), membrane_hp_thickness=30.0, auto_build=True):
+    def __init__(self, aas=(20, 20, 20, 20), radii=(5, 5, 5, 5), ah_rotations=(0, 0, 0, 0),
+                 sh_rotations=(0, 0, 0, 0), zshifts=(0, 0, 0, 0), tilt_angles=(0, 0, 0, 0),
+                 lmp_rotations=(0, 0, 0, 0),
+                 auto_build=True):
         """Generates a model of a membrane 4-helix bundle.
 
         Parameters
@@ -68,27 +69,17 @@ class Membrane4HelixBundle(Assembly):
         sh_rotations: [float, float, float, float]
             The rotation, in degrees of the helices about the super-helical
             axis.
-        aps: [bool, bool, bool, bool]
-            The orientation of the helices relative to the plane of the
-            membrane.
-        lmp_rotations: [float, float, float, float]
-            Local membrane perpendicular rotation is the rotation of the
-            helices about the vector perpendicular to the plane of the
-            membrane, running though the centre of the helix.
-        membrane_hp_thickness: float
-            The width of the hydrophobic region of the membrane.
         auto_build: bool
             Will automatically run the build method if true.
         """
         super().__init__()
         self.aas = list(aas)
-        self.raadii = list(radii)
+        self.radii = list(radii)
         self.ah_rotations = list(ah_rotations)
         self.sh_rotations = [x + y for x, y in zip(sh_rotations, self.default_sh_rot)]
-        self.aps = list(aps)
+        self.zshifts = list(zshifts)
+        self.tilt_angles = list(tilt_angles)
         self.lmp_rotations = list(lmp_rotations)
-        self.zshifts = zshifts
-        self.membrane_hp_thickness = membrane_hp_thickness
         if auto_build:
             self.build()
 
@@ -103,24 +94,17 @@ class Membrane4HelixBundle(Assembly):
 
         """
         self._molecules = []
-        parameters = (self.aas, self.raadii, self.ah_rotations,
-                      self.sh_rotations, self.aps, self.lmp_rotations, self.zshifts)
+        parameters = (self.aas, self.radii, self.ah_rotations,
+                      self.sh_rotations, self.zshifts, self.tilt_angles, self.lmp_rotations)
         if not all([len(x) == 4 for x in parameters]):
             raise ValueError('All parameter lists must have 4 values.')
-        for aa, radius, ah_rot, sh_rot, ap, lmp_rot, zshift in zip(*parameters):
+        for aa, radius, ah_rot, sh_rot, zshift, tilt_angle, lmp_rot in zip(*parameters):
             helix = Helix(aa)
             helix.rotate(ah_rot, (0, 0, 1))
             helix.translate((radius, 0, zshift))
             helix.rotate(sh_rot, (0, 0, 1), (0, 0, 0))
-            # Centre relative to plane of membrane
-            mem_adjust_vector = find_to_mem_plane_vector(
-                helix, self.membrane_hp_thickness)
-            helix.translate(mem_adjust_vector)
-            # Tilt Angle
-            tilt_angle = find_tilt_to_mem_angle(helix, self.membrane_hp_thickness)
-            tilt_angle = tilt_angle if not ap else tilt_angle + numpy.pi  # Adjust for AP
-            rotate_vector = helix.centre_of_mass - (0, 0, helix.centre_of_mass[2])
-            helix.rotate(tilt_angle, rotate_vector, helix.centre_of_mass, radians=True)
+            tilt_rotate_vector = helix.centre_of_mass - (0, 0, helix.centre_of_mass[2])
+            helix.rotate(tilt_angle, tilt_rotate_vector, helix.centre_of_mass)
             # Apply the local membrane perpendicular rotation
             helix.rotate(lmp_rot, (0, 0, 1), helix.centre_of_mass)
             self._molecules.append(helix)
