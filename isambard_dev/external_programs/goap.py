c589a9244b51b8ee232c1fe9da1e9ada8a322b44
Gail Bartlett <chgjb@bristol.ac.uk>
2017-01-11 14:07:25 +0000

    GOAP = Generalized orientation-dependent all-atom statistical potential for protein structure prediction. Zhou and Skolnick, Biophys J 101(8):2042-2052 (2011)

---
+++ isambard_dev/external_programs/goap.py
@@ -0,0 +1,47 @@
+import subprocess
+import tempfile
+from pathlib import Path
+
+from settings import global_settings
+
+def run_goap(input_file,path=True):
+
+    if path:
+        input_path=Path(input_file)
+        if not input_path.exists():
+            print('No file found at', path)
+            return None,None
+    else:
+        pathf = tempfile.NamedTemporaryFile()
+        encoded_input = input_file.encode()
+        pathf.write(encoded_input)
+        pathf.seek(0)
+        file_path=Path(file_path)
+        input_path = Path(file_path)
+
+    goap_dir = "/Users/chgjb/goap-alone"
+    goap_exe = "/Users/chgjb/goap-alone/goap"
+    goap_fh = tempfile.NamedTemporaryFile()
+    goap_input = "{}\n{}\n".format(goap_dir,input_file.name)
+    encoded_goap_input = goap_input.encode()
+    goap_fh.write(encoded_goap_input)
+    goap_fh.seek(0)
+
+    try:
+        goap_output = subprocess.run([str(goap_exe), stdin=goap_fh])
+
+    except:
+        FileNotFoundError as e:
+            print (e, '\nSomething went wrong')
+            return None
+
+    try:
+        goap_results = goap_output.decode()
+
+    except:
+
+        ValueError:
+            print ("No result")
+        return None
+
+    return goap_results
