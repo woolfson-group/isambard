02559c83d5326d7079a6cb1eeaf33d0c3447bea0
Chris Wells Wood <cwwoodesq@gmail.com>
2017-01-12 17:39:00 +0000

    Added doc strings.

--- isambard_dev/external_programs/dfire.py
+++ isambard_dev/external_programs/dfire.py
@@ -8,6 +8,7 @@ from tools.isambard_warnings import DependencyNotFoundWarning
 
 
 def check_dfire_avail():
+    """Test to see if dfire is available using the path to the binary in global settings."""
     is_dfire_available = False
     if os.path.isfile(global_settings['dfire']['path']):
         try:
@@ -28,6 +29,20 @@ global_settings['dfire']['available'] = check_dfire_avail()
 
 
 def run_dfire(pdb, path=True):
+    """Run dfire with a path to a pdb file or the contents of a PDB file as a string.
+
+    Parameters
+    ----------
+    pdb : str
+        PDB file or the contents of a PDB file as a string
+    path : bool
+        Indicates whether the `pdb` is a path or PDB format string.
+
+    Returns
+    -------
+    dfire_std_our : str
+        Output from dfire as a unicode string.
+    """
     if path:
         with open(pdb, 'r') as inf:
             pdb = inf.read()
@@ -44,10 +59,36 @@ def run_dfire(pdb, path=True):
 
 
 def parse_dfire_out(dfire_std_out):
+    """Parses the an output string produced by dfire.
+
+    Parameters
+    ----------
+    dfire_std_out : str
+        Output from dfire as a unicode string.
+
+    Returns
+    -------
+    dfire_score : float
+        The dfire score as a float.
+    """
     return float(dfire_std_out.split()[1])
 
 
 def calculate_score(pdb, path=True):
+    """Calculates the dfire score of the pdb file.
+
+    Parameters
+    ----------
+    pdb : str
+        PDB file or the contents of a PDB file as a string
+    path : bool
+        Indicates whether the `pdb` is a path or PDB format string.
+
+    Returns
+    -------
+    dfire_score : float
+        The dfire score as a float.
+    """
     if global_settings['dfire']['available'] is None:
         global_settings['dfire']['available'] = check_scwrl_avail()
     if not global_settings['dfire']['available']:
