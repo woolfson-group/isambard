1096039cd8d0ef5eea94326463e6fb5c8431bfb4
Chris Wells Wood <cwwoodesq@gmail.com>
2017-01-16 15:14:18 +0000

    Updated availability checking code.

--- isambard_dev/external_programs/dfire.py
+++ isambard_dev/external_programs/dfire.py
@@ -1,13 +1,12 @@
 import os
 import subprocess
 import tempfile
-import warnings
 
 from settings import global_settings
-from tools.isambard_warnings import DependencyNotFoundWarning
+from tools.isambard_warnings import check_availability
 
 
-def check_dfire_avail():
+def test_dfire():
     """Test to see if dfire is available using the path to the binary in global settings."""
     is_dfire_available = False
     if os.path.isfile(global_settings['dfire']['path']):
@@ -15,19 +14,10 @@ def check_dfire_avail():
             subprocess.check_output([global_settings['dfire']['path']], stderr=subprocess.DEVNULL)
         except subprocess.CalledProcessError:
             is_dfire_available = True
-    else:
-        warning_string = ('\n\nDfire not found and so cannot be used. Check that the path to the Dfire binary'
-                          ' in `.isambard_settings` is correct.\n'
-                          'Suggestion:\n'
-                          'You might want to try running isambard.settings.configure() after importing ISAMBARD in a\n'
-                          'Python interpreter or running `configure.py` in the module folder.')
-        warnings.warn(warning_string, DependencyNotFoundWarning)
     return is_dfire_available
 
 
-global_settings['dfire']['available'] = check_dfire_avail()
-
-
+@check_availability('dfire', test_dfire, global_settings)
 def run_dfire(pdb, path=True):
     """Run dfire with a path to a pdb file or the contents of a PDB file as a string.
 
@@ -89,13 +79,5 @@ def calculate_score(pdb, path=True):
     dfire_score : float
         The dfire score as a float.
     """
-    if global_settings['dfire']['available'] is None:
-        global_settings['dfire']['available'] = check_scwrl_avail()
-    if not global_settings['dfire']['available']:
-        warning_string = ('Dfire not found, score has not been calculated.\n'
-                          'Check that the path to the Scwrl binary in `.isambard_settings` is correct.\n'
-                          'You might want to try rerunning `configure.py`\n')
-        warnings.warn(warning_string, DependencyNotFoundWarning)
-        return
     dfire_out = run_dfire(pdb, path=path)
     return parse_dfire_out(dfire_out)
