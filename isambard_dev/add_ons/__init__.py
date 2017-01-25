267157d05d087dd839d8a8339738862c93896854
Chris Wells Wood <cwwoodesq@gmail.com>
2017-01-25 15:09:41 +0000

    Updated imports for add ons.

--- isambard_dev/add_ons/__init__.py
+++ isambard_dev/add_ons/__init__.py
@@ -1,3 +1,1 @@
-from add_ons import filesystem, pacc, plotting, knobs_into_holes
-from add_ons.notebook_visualiser import AMPALViewer
-from add_ons.parmed_to_ampal import convert_cif_to_ampal
+from add_ons import filesystem, pacc, plotting, knobs_into_holes, parmed_to_ampal
